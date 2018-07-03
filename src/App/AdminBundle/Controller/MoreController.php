17ef0eb7f1adb97927a2dacdd8e93ef651f5b608
Abdul Manan <39721062+mirza339@users.noreply.github.com>
2018-07-03 15:39:07 +0500

    about,contact us,slider

--- src/App/AdminBundle/Controller/MoreController.php
+++ src/App/AdminBundle/Controller/MoreController.php
@@ -9,12 +9,12 @@ class MoreController extends Controller
     public function AboutAction(Request $request){
         if ($request->getMethod() == 'POST'){
             $em = $this->getDoctrine()->getManager();
-            $about = $em->getRepository('DataBundle:More')->find(1);
+            $about = $em->getRepository('DataBundle:More')->findOneBy(array('name'=>'About'));
             $about->setText($request->get('text'));
             $em->flush();
             return $this->redirect($this->generateUrl('admin_info_about'));
         }
-        $about = $this->getDoctrine()->getRepository('DataBundle:More')->findBy(array('id'=>1));
+        $about = $this->getDoctrine()->getRepository('DataBundle:More')->findBy(array('name'=>'About'));
         return $this->render('AdminBundle:Info:about.html.twig',array(
             'about' => $about,
         ));
@@ -22,14 +22,14 @@ class MoreController extends Controller
     public function ContactAction(Request $request){
         if ($request->getMethod() == 'POST'){
             $em = $this->getDoctrine()->getManager();
-            $about = $em->getRepository('DataBundle:More')->find(2);
+            $about = $em->getRepository('DataBundle:More')->findOneBy(array('name'=>'Contact'));
             $about->setText($request->get('text'));
             $em->flush();
             return $this->redirect($this->generateUrl('admin_info_contact'));
         }
-        $about = $this->getDoctrine()->getRepository('DataBundle:More')->findBy(array('id'=>2));
+        $contact = $this->getDoctrine()->getRepository('DataBundle:More')->findBy(array('name'=>'Contact'));
         return $this->render('AdminBundle:Info:contact.html.twig',array(
-            'contact' => $about,
+            'contact' => $contact,
         ));
     }
 }
--- src/App/HomeBundle/Controller/InfoController.php
+++ src/App/HomeBundle/Controller/InfoController.php
@@ -7,13 +7,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class InfoController extends Controller
 {
     public  function aboutAction(){
-        $about = $this->getDoctrine()->getRepository('DataBundle:More')->findBy(array('id'=>1));
+        $about = $this->getDoctrine()->getRepository('DataBundle:More')->findBy(array('name'=>'About'));
         return $this->render('HomeBundle:Info:About.html.twig',array(
             'about' =>$about
         ));
     }
     public  function contactAction(){
-        $contact = $this->getDoctrine()->getRepository('DataBundle:More')->findBy(array('id'=>2));
+        $contact = $this->getDoctrine()->getRepository('DataBundle:More')->findBy(array('name'=>'Contact'));
         return $this->render('HomeBundle:Info:Contact.html.twig',array(
             'contact' =>$contact
         ));
